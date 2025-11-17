d3fc522907a1b52ec5d1cb320dc9752a887e967f
Plvkssh <kksmartregion@gmail.com>
2025-11-17 21:14:24 +0700

    Create Tag.java

---
+++ src/main/java/com/example/lms/model /Tag.java
@@ -0,0 +1,54 @@
+package com.example.lms.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tags", uniqueConstraints = {
+        @UniqueConstraint(name = "uk_tag_name", columnNames = {"name"})
+})
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "tags")
+    private Set<Course> courses = new HashSet<>();
+
+    // Конструкторы
+    public Tag() {}
+    
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    // Геттеры и сеттеры
+    public Long getId() { 
+        return id; 
+    }
+    
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+    
+    public String getName() { 
+        return name; 
+    }
+    
+    public void setName(String name) { 
+        this.name = name; 
+    }
+    
+    public Set<Course> getCourses() { 
+        return courses; 
+    }
+    
+    public void setCourses(Set<Course> courses) { 
+        this.courses = courses; 
+    }
+}
