49d5db059074b756be4c908fda59be19216a38ee
Plvkssh <kksmartregion@gmail.com>
2025-12-24 14:58:51 +0700

    Update AssignmentRepository.java

--- src/main/java/com/example/lms/repository /AssignmentRepository.java
+++ src/main/java/com/example/lms/repository /AssignmentRepository.java
@@ -6,17 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-/**
- * Репозиторий для работы с заданиями в системе обучения.
- * Предоставляет стандартные CRUD-операции через JpaRepository.
- */
 public interface AssignmentRepository extends JpaRepository<Assignment, Long> {
     
-    /**
-     * Находит все задания, принадлежащие конкретному уроку.
-     *
-     * @param lesson урок, для которого нужно найти задания
-     * @return список заданий урока
-     */
     List<Assignment> findByLesson(Lesson lesson);
 }
