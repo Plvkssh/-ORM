8786da63fc1edfdd6617116d815a918e266f7b2a
Plvkssh <kksmartregion@gmail.com>
2025-12-24 14:58:26 +0700

    Update AnswerOptionRepository.java

--- src/main/java/com/example/lms/repository /AnswerOptionRepository.java
+++ src/main/java/com/example/lms/repository /AnswerOptionRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AnswerOptionRepository extends JpaRepository<AnswerOption, Long> {
+    
     List<AnswerOption> findByQuestion(Question question);
 }
